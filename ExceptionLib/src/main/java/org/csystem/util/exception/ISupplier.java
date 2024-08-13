3e567c53bac42000339ad082f086575b72748a59
ibrah <0ibrahimyalcin6@gmail.com>
2024-08-13 21:28:03 +0300

    update - Added Javadoc comments for the methods `ISupplier`, `IAction`

--- ExceptionLib/src/main/java/org/csystem/util/exception/ISupplier.java
+++ ExceptionLib/src/main/java/org/csystem/util/exception/ISupplier.java
@@ -10,6 +10,14 @@ All Rights Free
 -----------------------------------------------------------------------*/
 package org.csystem.util.exception;
 
+/**
+ * A functional interface that represents a supplier of results.
+ * This interface has a single abstract method {@code get()} that supplies a result of type {@code R}.
+ * The method can throw an {@code Exception} if an error occurs during the result generation.
+ * @param <R> the type of result supplied by this supplier
+ * @see Exception
+ */
+
 @FunctionalInterface
 public interface ISupplier<R> {
     R get() throws Exception;
