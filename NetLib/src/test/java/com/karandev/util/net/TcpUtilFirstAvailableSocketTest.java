c3fa24c3f9f2154f984bdc7e743e930cc92733a7
diodeFunk <119134491+diodeFunk@users.noreply.github.com>
2024-08-14 11:52:37 +0300

    Update TcpUtilFirstAvailableSocketTest

--- NetLib/src/test/java/com/karandev/util/net/TcpUtilFirstAvailableSocketTest.java
+++ NetLib/src/test/java/com/karandev/util/net/TcpUtilFirstAvailableSocketTest.java
@@ -1,20 +1,44 @@
 package com.karandev.util.net;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 @Disabled("Run the debug test")
 public class TcpUtilFirstAvailableSocketTest {
-    @Test
-    public void test() throws IOException
+    private void presentCallback(ServerSocket serverSocket)
     {
-        var serverSocketOpt = TcpUtil.getFirstAvailableSocket(512, 1024, 65535);
-        System.out.println(serverSocketOpt.get().getLocalPort());
+        Assertions.fail();
+    }
+
+    @Test
+    public void givenPortNumberRange_whenAvailable_thenPortAssigned()
+    {
+        var serverSocketOpt = TcpUtil.getFirstAvailableSocket(1, 1024, 65535);
+
         Assertions.assertTrue(serverSocketOpt.isPresent());
+        var localPort = serverSocketOpt.get().getLocalPort();
+
+        Assertions.assertTrue(IntStream.rangeClosed(1024, 65535)
+                .anyMatch(i -> i == localPort));
+
+        System.out.printf("Assigned Port: %s%n", localPort);
+    }
+
+    @Test
+    public void givenPortNumber_whenPortIsUsed_thenReturnEmptyOptional() throws IOException
+    {
+        int[] port = {6666};
+
+        try (var busySocket = new ServerSocket(6666)) {
+            busySocket.setSoTimeout(5000);
+
+            var serverSocketOpt = TcpUtil.getFirstAvailableSocket(1, port);
+            serverSocketOpt.ifPresent(this::presentCallback);
+        }
     }
 }
