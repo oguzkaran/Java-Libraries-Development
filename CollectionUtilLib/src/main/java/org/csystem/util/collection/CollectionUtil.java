f6ea2e9dc7c5a4f1bb800b3301090ba2d5e3d478
Oğuz Karan <oguzkaran@csystem.org>
2024-08-28 20:33:30 +0300

    update for project as a skeleton

--- CollectionUtilLib/src/main/java/org/csystem/util/collection/CollectionUtil.java
+++ CollectionUtilLib/src/main/java/org/csystem/util/collection/CollectionUtil.java
@@ -1,61 +1,53 @@
-/*----------------------------------------------------------------------
-	FILE        : CollectionUtil.java
-	AUTHOR      : Oğuz Karan
-	LAST UPDATE : 14.08.2024
-
-	Utility class for collections
-
-	Copyleft (c) 1993 by C and System Programmers Association (CSD)
-	All Rights Free
------------------------------------------------------------------------*/
 package org.csystem.util.collection;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import java.util.Map;
 
 public final class CollectionUtil {
     private CollectionUtil()
     {
     }
 
-    public static <T> boolean areAllDistinct(Collection<? extends T> collection)
+    // Adds an element to the collection unless the element is null.
+    public static <E> boolean addIfNotNull(Collection<E> collection, E object)
     {
-        return collection.size() == new HashSet<T>(collection).size();
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public static <T, R> List<R> toList(Iterable<T> iterable, Function<? super T, R> func)
+    public static <E> boolean areAllDistinct(Collection<? extends E> collection)
     {
-        return toList(iterable, func, false);
+        return collection.size() == new HashSet<E>(collection).size();
     }
 
-    public static <T, R> Iterable<R> toIterable(Iterable<T> iterable, Function<? super T, R> func)
+    public static <E> boolean contains(Collection<E> iterable, Object object)
     {
-        return toIterable(iterable, func, false);
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public static <T, R> List<R> toList(Iterable<T> iterable, Function<? super T, R> func, boolean parallel)
+    // Extract the lone element of the specified Collection
+    //Can be used as ImmutableSingleton Collections.singleton(collection.iterator().next());
+    public static <E> E extractSingleton(Collection<E> collection)
     {
-        return StreamSupport.stream(iterable.spliterator(), parallel).map(func).collect(Collectors.toList());
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public static <T, R> Iterable<R> toIterable(Iterable<T> iterable, Function<? super T, R> func, boolean parallel)
+    //It is used to search for a component of a specific type within collections containing multiple types.
+    public static Object findValueOfType(Collection<?> collection, Class<?>[] types)
     {
-        return toList(iterable, func, parallel);
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public static <T> void forEach(Iterable<T> iterable, Consumer<? super T> consumer)
+    public static <K, V> Map.Entry<K, V> get(Map<K, V> map, int index)
     {
-        forEach(iterable, consumer, false);
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public static <T> void forEach(Iterable<T> iterable, Consumer<? super T> consumer, boolean parallel)
+    // Removes elements whose index are between startIndex, inclusive, and endIndex.
+    public static <E> Collection<E> removeCount(Collection<E> input, int startIndex, int count)
     {
-        StreamSupport.stream(iterable.spliterator(), parallel).forEach(consumer);
+        throw new UnsupportedOperationException("Not yet implemented");
     }
+
     //...
 }
