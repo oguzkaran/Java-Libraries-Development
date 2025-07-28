d180b23ba66084877600487fc873b1328a573d28
diodeFunk <119134491+diodeFunk@users.noreply.github.com>
2025-07-28 21:09:38 +0300

    Add documentation to classes and interfaces

--- ConverterLib/src/main/java/org/csystem/util/converter/IIntConverter.java
+++ ConverterLib/src/main/java/org/csystem/util/converter/IIntConverter.java
@@ -1,9 +1,31 @@
 package org.csystem.util.converter;
 
+/**
+ * Converter functional interface for converting an int value to another type.
+ *
+ * @param <R> the result type
+ * @author JavaApp2-Jan-2024 Group
+ * @version 2.0.0
+ */
 @FunctionalInterface
 public interface IIntConverter<R> {
+    /**
+     * Converts an int value to type R.
+     *
+     * @param val the int value to convert
+     * @return the converted value
+     * @throws Exception if conversion fails
+     */
     R convert(int val) throws Exception;
 
+    /**
+     * Converts an object of type R back to an int value.
+     * By default, this operation is not supported and throws an exception.
+     *
+     * @param r the object to convert back
+     * @return the converted int value
+     * @throws Exception if conversion fails or not supported
+     */
     default int convertBack(R r) throws Exception
     {
         throw new UnsupportedOperationException("convertBack not supported");
--- ConverterLib/src/main/java/org/csystem/util/converter/IStringConverter.java
+++ ConverterLib/src/main/java/org/csystem/util/converter/IStringConverter.java
@@ -1,5 +1,12 @@
 package org.csystem.util.converter;
 
+/**
+ * Converter functional interface for converting a String to another type.
+ *
+ * @param <R> the result type
+ * @author JavaApp2-Jan-2024 Group
+ * @version 2.0.0
+ */
 @FunctionalInterface
 public interface IStringConverter<R> extends IConverter<String, R> {
 }
